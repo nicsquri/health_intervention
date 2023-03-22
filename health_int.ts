f51155d8373e65f71f59b10cc99977520fd3b26a
nicsquri <128560913+nicsquri@users.noreply.github.com>
2023-03-22 10:37:03 +0800

    Update health_int.ts

--- health_int.ts
+++ health_int.ts
@@ -59,7 +59,71 @@ function getCatalog(): Catalog {
       name: "Whole Grain Bread (1 slice)",
       clusterId: "interventions",
     },
-    // Add more interventions here
+    {
+      id: "intervention-11",
+      name: "Drinking Water (500mL)",
+      clusterId: "interventions",
+    },
+     {
+      id: "intervention-12",
+      name: "Drinking Green Tea (1cup)",
+      clusterId: "interventions",
+    },
+     {
+      id: "intervention-13",
+      name: "Taking Turmeric (1tsp)",
+      clusterId: "interventions",
+    },
+     {
+      id: "intervention-14",
+      name: "Eating Blueberries (1cup)",
+      clusterId: "interventions",
+    },
+     {
+      id: "intervention-15",
+      name: "Having extra primary sleep (10min)",
+      clusterId: "interventions",
+    },
+     {
+      id: "intervention-16",
+      name: "Laughing (5min)",
+      clusterId: "interventions",
+    },
+    {
+      id: "intervention-17",
+      name: "Social Interaction (30min)",
+      clusterId: "interventions",
+    },
+    {
+      id: "intervention-18",
+      name: "Washing hands (30sec)",
+      clusterId: "interventions",
+    },
+    {
+      id: "intervention-19",
+      name: "Wearing Face Mask (1day)",
+      clusterId: "interventions",
+    },
+    {
+      id: "intervention-20",
+      name: "Brushing Teeth (2min)",
+      clusterId: "interventions",
+    },
+    {
+      id: "intervention-21",
+      name: "Flossing (1time)",
+      clusterId: "interventions",
+    },
+    {
+      id: "intervention-22",
+      name: "Sunscreen application",
+      clusterId: "interventions",
+    },
+    {
+      id: "intervention-23",
+      name: "Eating brown sugar (10g)",
+      clusterId: "interventions",
+    },
   ];
 
   return { items };
